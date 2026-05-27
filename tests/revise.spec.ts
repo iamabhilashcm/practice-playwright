313db9056c98a8d8c4b05d26167e00923d30e1eb
iamabhilashcm <iamabhilashcm@gmail.com>
2026-05-27 16:09:32 +0530

    file name changes

--- tests/revise.spec.ts
+++ tests/revise.spec.ts
@@ -1,32 +1,31 @@
-import {expect, test} from '@playwright/test'
+import { expect, test } from "@playwright/test";
 
-test('By user search', async({page})=>{
+test("By user search", async ({ page }) => {
+  await page.goto("https://www.amazon.in/");
 
-    await page.goto('https://www.amazon.in/')
+  await page.getByPlaceholder("Search Amazon.in").fill("mobile 5g");
 
-    await page.getByPlaceholder('Search Amazon.in').fill('mobile 5g')
+  await page.keyboard.press("Enter");
 
-    await page.keyboard.press('Enter')
+  await expect(page.getByText('results for "mobile 5g"')).toBeVisible();
 
-    await expect(page.getByText('results for "mobile 5g"')).toBeVisible()
-
-    await page.getByLabel('Apply the filter Samsung to narrow results').click()
-
+  await page.getByLabel("Apply the filter Samsung to narrow results").click();
 });
 
-test('By locator search', async({page})=>{
+test("By locator search", async ({ page }) => {
+  await page.goto("https://www.amazon.in/");
 
-    await page.goto('https://www.amazon.in/')
+  await page.getByPlaceholder("Search Amazon.in").fill("mobile 5g");
 
-    await page.getByPlaceholder('Search Amazon.in').fill('mobile 5g')
+  await page.locator("#nav-search-submit-button").click();
 
-    await page.locator('#nav-search-submit-button').click()
-    
-    //or 
-    //await page.keyboard.press('Enter')
+  //or
+  //await page.keyboard.press('Enter')
 
-    await expect(page.getByText('results for "mobile 5g"')).toBeVisible()
+  await expect(page.getByText('results for "mobile 5g"')).toBeVisible();
+});
 
-   
-
+test("fluke site", async ({page}) => {
+  await page.goto("https://www.fluke.com/en-us");
+  await page.waitForLoadState();
 });
