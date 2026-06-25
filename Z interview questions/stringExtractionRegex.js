a872a958228ced8c6fc008dcd0ab5226e8f2be2f
iamabhilash-pc <iamabhilashcm@gmail.com>
2026-06-25 18:26:17 +0530

    updated questions ans

--- Z interview questions/stringExtractionRegex.js
+++ Z interview questions/stringExtractionRegex.js
@@ -4,6 +4,9 @@ const string = "Hell0 @World!#2026";
 const onlyChars = string.replace(/[^a-zA-Z]/g, "");
 
 console.log(onlyChars); // "HellWorld" (output)
+//
+//
+//
 
 // method 2 with unicodes
 
@@ -14,3 +17,15 @@ const result = string2.replace(/[^\p{L}]/gu, "");
 console.log(result); // "Héllo世界" (output)
 
 const string3 = "#QuantumTiger88$:";
+//
+//
+//
+
+// Printing the partial String
+
+const str = "Abhi123!@#XYZ789$%^";
+
+const stringOne = str.match(/[A-Za-z]+/g);
+
+console.log(stringOne[0]); // Abhi
+console.log(stringOne[1]); // XYZ
